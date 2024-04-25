e23769935379fa4bedcacbe2b113ad50e64a4b2e
Joas Fyllipe <joassolano2908@gmail.com>
2024-04-25 13:11:36 -0300

    Tipos De Dados C#

--- ExemplosFundamentos/TiposDeDados/Program.cs
+++ ExemplosFundamentos/TiposDeDados/Program.cs
@@ -1,33 +1,44 @@
-﻿using ExemplosFundamentos.Models;
+﻿using System.Data;
+using ExemplosFundamentos.Models;
+
+
+// string - cadeia de caracteres;
+// int - tipo numerico inteiro;
+// bool - verdadeiro ou falso;
+// decimal - representar valores decimais monetarios "M";
+// double - valor decimal;
+// char - caractere unicode;
+// DataTime - representação de datas, horas, manipular datas;
+
+
 
 DateTime dataAtual = DateTime.Now.AddDays(5);
 Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
 
-// string apresentacao = "Olá, seja bem vindo";
+ string apresentacao = "Olá, seja bem vindo";
 
-// int quantidade = 1;
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+ int quantidade = 1;
+ Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
-// quantidade = 10;
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+ quantidade = 10;
+ Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
-// double altura = 1.80;
+ double altura = 1.80;
 
-// decimal preco = 1.80M;
+ decimal preco = 1.80M;
 
-// bool condicao = true;
+ bool condicao = true;
 
 
 
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-// Console.WriteLine("Valor da variável preco: " + preco);
-// Console.WriteLine("Valor da variável condicao: " + condicao);
+ Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+ Console.WriteLine("Valor da variável preco: " + preco);
+ Console.WriteLine("Valor da variável condicao: " + condicao);
 
-//  Pessoa pessoa1 = new Pessoa ();
-// pessoa1.Nome = "Joás";
-// pessoa1.Idade = 19;
-// pessoa1.Apresentar();
+  Pessoa pessoa1 = new Pessoa ();
+ pessoa1.Nome = "Joás";
+ pessoa1.Idade = 19;
+ pessoa1.Apresentar();
 
-// Pessoa _pessoa_FisicaRepresentacao = new Pessoa();
+ Pessoa _pessoa_FisicaRepresentacao = new Pessoa();
